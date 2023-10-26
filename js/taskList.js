8d66d55f72f16baa61dd867c1975fe2e860865af
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-10-26 11:55:58 +0200

    JS function changed

--- js/taskList.js
+++ js/taskList.js
@@ -14,17 +14,43 @@
         let htmlstring = "";
         for (const task of tasks) {
         htmlstring +=  `
-        <li>
-            ${task.content}
+        <li
+        ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+        ${task.content}
         </li>
         `;  
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlstring;
-    }
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if(newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
     };
 
     init();
