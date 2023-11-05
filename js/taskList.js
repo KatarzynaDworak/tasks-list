06e6ab6ddeb016c5a6b45776d712da01b2dfc8de
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-11-05 13:47:46 +0100

    li class in js changed

--- js/taskList.js
+++ js/taskList.js
@@ -46,7 +46,7 @@
     for (const task of tasks) {
         tasksListHTMLContent +=  `
         
-        <li 
+        <li class="task__item "
         ${task.done ? " style=\"text-decoration: line-through\"" : ""}
         >
         <button class="tasks__button tasks__button--toggleDone js-toggleDone">${task.done ? "✓" : ""}</button>
