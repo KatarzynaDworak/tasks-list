4fb17088be98187d00a088933b5235c4bc1d968d
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-11-09 18:35:55 +0100

    code formatting

--- js/taskList.js
+++ js/taskList.js
@@ -46,13 +46,13 @@
             tasksHTMLContent += `
         <li class="tasks__item js-tasks">
             <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-            ${task.done ? "✓" : ""}
+                ${task.done ? "✓" : ""}
             </button>
             <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
-            ${task.content}
+                ${task.content}
             </span>
             <button class="tasks__button tasks__button--remove js-remove">
-            🗑️
+                🗑️
             </button>
         </li>
         `;
